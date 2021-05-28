e77f16bd8b53b2b6f05cb791f2de967820b442f2
0miguel <miguelbarroe@gmail.com>
2021-05-27 21:41:57 -0500

    create mesh matrix

--- cube.js
+++ cube.js
@@ -6,7 +6,22 @@ import { OrbitControls } from "https://unpkg.com/three@0.124.0/examples/jsm/cont
 let renderer, scene, camera, cameraControls;
 let dimensions, f0, f1, f2, f3, f4, f5;
 let rotationQ = [];
-let currentRotation, rotationCounter = 0, rotating = false, front;
+let currentRotation, rubiksMesh, rotationCounter = 0, rotating = false;
+
+const moves = {
+	U: "U",
+	UP: "U'",
+	D: "D",
+	DP: "D'",
+	R: "R",
+	RP: "R'",
+	L: "L",
+	LP: "L'",
+	F: "F",
+	FP: "F'",
+	B: "B",
+	BP: "B'",
+}
 
 THREE.Object3D.prototype.rotateAroundWorldAxis = function() {
     var q = new THREE.Quaternion();
@@ -41,9 +56,23 @@ function init() {
 
 	scene.add(new THREE.AmbientLight(0xffffff));
 
+	rubiksMesh = createMesh();
 	rubik(dimensions, f0, f1, f2, f3, f4, f5);
 
 	renderLoop();
+}
+
+function createMesh() {
+	rubiksMesh = [[], [], [], [], [], []];
+	for(let i = 0; i < 6; i++) {
+		for(let j = 0; j < dimensions; j++) {
+			rubiksMesh[i].push([]);
+			for(let k = 0; k < dimensions; k++) {
+				rubiksMesh[i][j].push(0);
+			}
+		}
+	}
+	return rubiksMesh;
 }
 
 function renderLoop() {
@@ -55,7 +84,108 @@ function renderLoop() {
 }
 
 function renderMove(move) {
-	console.log(move);
+	const size = dimensions - 1;
+	let temp;
+	switch(move) {
+		case moves.U:
+			cube[4] = rotateClockwise(cube[4]);
+			temp = cube[0][0];
+			cube[0][0] = cube[1][0];
+			cube[1][0] = cube[2][0];
+			cube[2][0] = cube[3][0];
+			cube[3][0] = temp;
+			break;
+		case moves.UP:
+			cube[4] = rotateCounterClockwise(cube[4]);
+			temp = cube[0][0];
+			cube[0][0] = cube[3][0];
+			cube[3][0] = cube[2][0];
+			cube[2][0] = cube[1][0];
+			cube[1][0] = temp;
+			break;
+		case moves.D:
+			cube[5] = rotateClockwise(cube[5]);
+			temp = cube[0][size];
+			cube[0][size] = cube[3][size];
+			cube[3][size] = cube[2][size];
+			cube[2][size] = cube[1][size];
+			cube[1][size] = temp;
+			break;
+		case moves.DP:
+			cube[5] = rotateCounterClockwise(cube[5]);
+			temp = cube[0][size];
+			cube[0][size] = cube[1][size];
+			cube[1][size] = cube[2][size];
+			cube[2][size] = cube[3][size];
+			cube[3][size] = temp;
+			break;
+		case moves.R:
+			cube[1] = rotateClockwise(cube[1]);
+			temp = getColumn(cube[0], size);
+			setColumn(cube[0], size, getColumn(cube[5], size));
+			setColumn(cube[5], size, getColumn(cube[2], 0).reverse());
+			setColumn(cube[2], 0, getColumn(cube[4], size).reverse());
+			setColumn(cube[4], size, temp);
+			break;
+		case moves.RP:
+			cube[1] = rotateCounterClockwise(cube[1]);
+			temp = getColumn(cube[0], size);
+			setColumn(cube[0], size, getColumn(cube[4], size));
+			setColumn(cube[4], size, getColumn(cube[2], 0).reverse());
+			setColumn(cube[2], 0, getColumn(cube[5], size).reverse());
+			setColumn(cube[5], size, temp);
+			break;
+		case moves.L:
+			cube[3] = rotateClockwise(cube[3]);
+			temp = getColumn(cube[0], 0);
+			setColumn(cube[0], 0, getColumn(cube[4], 0));
+			setColumn(cube[4], 0, getColumn(cube[2], size).reverse());
+			setColumn(cube[2], size, getColumn(cube[5], 0).reverse());
+			setColumn(cube[5], 0, temp);
+			break;
+		case moves.LP:
+			cube[3] = rotateCounterClockwise(cube[3]);
+			temp = getColumn(cube[0], 0);
+			setColumn(cube[0], 0, getColumn(cube[5], 0));
+			setColumn(cube[5], 0, getColumn(cube[2], size).reverse());
+			setColumn(cube[2], size, getColumn(cube[4], 0).reverse());
+			setColumn(cube[4], 0, temp);
+			break;
+		case moves.F:
+			cube[0] = rotateClockwise(cube[0]);
+			temp = cube[4][size];
+			cube[4][size] = getColumn(cube[3], size).reverse();
+			setColumn(cube[3], size, cube[5][0]);
+			cube[5][0] = getColumn(cube[1], 0).reverse();
+			setColumn(cube[1], 0, temp);
+			break;
+		case moves.FP:
+			cube[0] = rotateCounterClockwise(cube[0]);
+			temp = cube[4][size];
+			cube[4][size] = getColumn(cube[1], 0);
+			setColumn(cube[1], 0, cube[5][0].slice().reverse());
+			cube[5][0] = getColumn(cube[3], size);
+			setColumn(cube[3], size, temp.slice().reverse());
+			break;
+		case moves.B:
+			cube[2] = rotateClockwise(cube[2]);
+			temp = cube[4][0];
+			cube[4][0] = getColumn(cube[1], size);
+			setColumn(cube[1], size, cube[5][size].slice().reverse());
+			cube[5][size] = getColumn(cube[3], 0);
+			setColumn(cube[3], 0, temp.slice().reverse());
+			break;
+		case moves.BP:
+			cube[2] = rotateCounterClockwise(cube[2]);
+			temp = cube[4][0];
+			cube[4][0] = getColumn(cube[3], 0).reverse();
+			setColumn(cube[3], 0, cube[5][size]);
+			cube[5][size] = getColumn(cube[1], size).reverse();
+			setColumn(cube[1], size, temp);
+			break;
+		default:
+			console.log("ERROR: invalid move");
+	}
 }
 
 function rubik(dimensions, f0, f1, f2, f3, f4, f5) {
@@ -80,7 +210,6 @@ function rubik(dimensions, f0, f1, f2, f3, f4, f5) {
 		return cube;
 	}
 
-	front = [];
 	let c;
 	let offset = (dimensions - 1) / 2;
 	for(let i = 0; i < dimensions; i ++) {
@@ -91,14 +220,38 @@ function rubik(dimensions, f0, f1, f2, f3, f4, f5) {
 					z = (k - offset) * increment;
 
 				c = newCube(x, y, z);
-				console.log(i, j, k);
-				if(k == dimensions - 1) {
-					front.push(c);
+				let coords = getCoords(i, j, k);
+				for(let m = 0; m < coords.length; m++) {
+					addCube(c, coords[m][0], coords[m][1], coords[m][2])				
 				}
 			}
 		}
 	}
-	addRotation(front, true, "z", 5);
+	// addRotation(front, true, "z", 5);
+}
+
+function getCoords(i, j, k) {
+	let res = [];
+	if(i == 0) {
+		res.push([3, dimensions - j - 1, dimensions - k - 1]);
+	} else if(i == dimensions - 1) {
+		res.push([1, dimensions - j - 1, k]);
+	}
+	if(j == 0) {
+		res.push([5, k, i]);
+	} else if(j == dimensions - 1) {
+		res.push([4, dimensions - k - 1, i]);
+	}
+	if(k == 0) {
+		res.push([0, dimensions - j - 1, i]);
+	} else if(k == dimensions - 1) {
+		res.push([2, dimensions - j - 1, dimensions - i - 1]);
+	}
+	return res;
+}
+
+function addCube(cube, face, i, j) {
+	rubiksMesh[face][i][j] = cube;
 }
 
 function addRotation(face, cw, axis, speed) {
